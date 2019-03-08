10b901562c01a5b9b7b5d1df775dd315253c1957
Aleksandar Gakovic <algakovic@gmail.com>
2019-03-08 13:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+function moveDodgerRight(){
+	var rightNumbers = dodger.style.left.replace('px', '')
+	var right = parseInt(rightNumbers, 10)
+	if (right < 360) {
+		dodger.style.left = `${right - 1}px`
+    }
+}
+	
+undefined
+function moveDodgerRight(){
+	var rightNumbers = dodger.style.left.replace('px', '')
+	var right = parseInt(rightNumbers, 10)
+	if (right < 360) {
+		dodger.style.left = `${right + 1}px`
+    }
+}
+	
